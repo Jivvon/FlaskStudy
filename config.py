7dd7860e9cf13a1a0a97657cbda5696da2e66d58
Jiwon Jeong <poiu8944@gmail.com>
2020-12-30 16:29:18 +0900

    Flask form 모듈 및 validator 사용

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False # SQLAlchemy의 이벤트를 처리하는 옵션 (현재 프로젝트에서 필요하지 않음)
+
+SECRET_KEY = "dev"
